8a931d1b4cf70fbe0d877d56b93a5467c7a66283
SmileJun <godhkekf24@inha.edu>
2023-06-27 14:49:16 +0900

    [DAD-299] chore: 프로젝트 기본 설정 파일 추가

---
+++ src/main/java/com/forever/dadamda/DadamdaApplication.java
@@ -0,0 +1,13 @@
+package com.forever.dadamda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadamdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DadamdaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/forever/dadamda/DadamdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forever.dadamda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadamdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
