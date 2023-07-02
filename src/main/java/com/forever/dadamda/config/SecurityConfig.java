4242e65bf168877b496c2705abb7f0a12e7dd84f
HanDaYeon-coder <hdy78901@naver.com>
2023-07-02 16:45:32 +0900

    [DAD-339] feat: Spring Security에 JWT 토큰 관련 사항 추가

--- src/main/java/com/forever/dadamda/config/SecurityConfig.java
+++ src/main/java/com/forever/dadamda/config/SecurityConfig.java
@@ -2,17 +2,21 @@ package com.forever.dadamda.config;
 
 import com.forever.dadamda.entity.Role;
 import com.forever.dadamda.service.CustomOAuth2UserService;
+import com.forever.dadamda.service.TokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
+    private final TokenService tokenService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -20,19 +24,22 @@ public class SecurityConfig {
          http
                 .csrf().disable()
                 .headers().frameOptions().disable()
-                .and()
-                .authorizeRequests()
-                .antMatchers("/", "/css/**", "/images/**",
+                 .and()
+                    .authorizeRequests()
+                        .antMatchers("/", "/css/**", "/images/**",
                         "/js/**","/h2-console/**").permitAll()
-                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
-                .anyRequest().authenticated()
-                .and()
-                .logout()
-                .logoutSuccessUrl("/")
-                .and()
-                .oauth2Login()
-                .userInfoEndpoint()
-                .userService(customOAuth2UserService);
+                        .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                        .anyRequest().authenticated()
+                 .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                 .and()
+                    .addFilterBefore(new JwtAuthFilter(tokenService),
+                            UsernamePasswordAuthenticationFilter.class)
+                    .oauth2Login()
+                        .successHandler(oAuth2SuccessHandler)
+                            .userInfoEndpoint()
+                            .userService(customOAuth2UserService);
 
          return http.build();
     }
