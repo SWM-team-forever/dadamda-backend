89daa2bc204965a6706b895f8d78202d8c3d1e1d
SmileJun <godhkekf24@inha.edu>
2023-07-05 19:46:35 +0900

    [DAD-332] fix: 테스트 어노테이션 주석 처리

--- src/test/java/com/forever/dadamda/DadamdaApplicationTests.java
+++ src/test/java/com/forever/dadamda/DadamdaApplicationTests.java
@@ -3,7 +3,7 @@ package com.forever.dadamda;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class DadamdaApplicationTests {
 
     @Test
