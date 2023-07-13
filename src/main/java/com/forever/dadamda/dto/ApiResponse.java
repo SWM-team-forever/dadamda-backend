584bafdabaa17466e161260271dd600a318bf327
HanDaYeon-coder <hdy78901@naver.com>
2023-07-13 19:23:29 +0900

    [DAD-385] feat: Api 응답 형식의 템플릿 추가

---
+++ src/main/java/com/forever/dadamda/dto/ApiResponse.java
@@ -0,0 +1,31 @@
+package com.forever.dadamda.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    public static final ApiResponse<String> OK = success("OK");
+
+    private String resultCode;
+
+    private String message;
+
+    private T data;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>("", "", data);
+    }
+
+    public static <T> ApiResponse<T> error(ErrorCode errorCode) {
+        return new ApiResponse<>(errorCode.getCode(), errorCode.getMessage(), null);
+    }
+
+    public static <T> ApiResponse<T> error(ErrorCode errorCode, String message) {
+        return new ApiResponse<>(errorCode.getCode(), message, null);
+    }
+}
