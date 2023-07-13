d42f9fb145a52c83940cb2ccfdfe2266e97a03f7
HanDaYeon-coder <hdy78901@naver.com>
2023-07-13 19:20:26 +0900

    [DAD-385] feat: ErrorCode 추가

---
+++ src/main/java/com/forever/dadamda/dto/ErrorCode.java
@@ -0,0 +1,32 @@
+package com.forever.dadamda.dto;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+    /**
+     * 400 Bad Request (잘못된 요청)
+     */
+    INVALID("BR000", "잘못된 요청입니다."),
+    INVALID_AUTH_TOKEN("BR001", "만료된 엑세스 토큰입니다."),
+
+    /**
+     * 404 Not Found (존재하지 않는 리소스)
+     */
+    NOT_EXISTS("NF000", "존재하지 않습니다."),
+    NOT_EXISTS_SCRAP("NF001", "존재하지 않는 스크랩입니다."),
+
+    /**
+     * 500 Internal Server Exception (서버 내부 에러)
+     */
+    INTERNAL_SERVER("IS000", "서버 내부 에러가 발생했습니다."),
+    ;
+
+    private final String code;
+    private final String message;
+
+    ErrorCode(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
