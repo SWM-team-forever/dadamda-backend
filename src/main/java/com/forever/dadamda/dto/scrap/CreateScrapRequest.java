615123d41437bec9b0c6723a6c611f6029b70426
HanDaYeon-coder <hdy78901@naver.com>
2023-07-18 13:22:37 +0900

    [DAD-387] refactor: url validation 어노테이션 변경

--- src/main/java/com/forever/dadamda/dto/scrap/CreateScrapRequest.java
+++ src/main/java/com/forever/dadamda/dto/scrap/CreateScrapRequest.java
@@ -1,12 +1,12 @@
 package com.forever.dadamda.dto.scrap;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -14,8 +14,7 @@ import lombok.NoArgsConstructor;
 public class CreateScrapRequest {
 
     @NotBlank(message = "url을 입력해주세요.")
-    @Pattern(regexp = "^((http(s?))\\:\\/\\/)([0-9a-zA-Z\\-]+\\.)+[a-zA-Z]{2,6}(\\:[0-9]+)?(\\/\\S*)?$",
-            message = "Link 형식이 유효하지 않습니다.")
+    @URL(message = "URL 형식이 유효하지 않습니다.")
     @Size(max = 2083, message = "최대 2083자까지 입력할 수 있습니다.")
     private String pageUrl;
 }
