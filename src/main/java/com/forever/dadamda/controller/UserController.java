600bcedb67ae10abee048de62087057905df2b4f
HanDaYeon-coder <hdy78901@naver.com>
2023-07-26 13:44:41 +0900

    [DAD-423] refactor: 로그인 응답값 email에서 profileUrl로 변경

--- src/main/java/com/forever/dadamda/controller/UserController.java
+++ src/main/java/com/forever/dadamda/controller/UserController.java
@@ -2,9 +2,8 @@ package com.forever.dadamda.controller;
 
 import com.forever.dadamda.dto.user.LoginResponse;
 import com.forever.dadamda.service.TokenService;
+import com.forever.dadamda.service.user.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 public class UserController {
+
     private final TokenService tokenService;
+    private final UserService userService;
 
     @GetMapping("")
     public LoginResponse loginResponse(@RequestParam String token) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String username = authentication.getName();
-        String email = tokenService.getEmail(token);
-        return LoginResponse.of(email);
+
+        String profileUrl = userService.getProfileUrl(tokenService.getEmail(token));
+
+        return LoginResponse.of(profileUrl);
     }
 }
