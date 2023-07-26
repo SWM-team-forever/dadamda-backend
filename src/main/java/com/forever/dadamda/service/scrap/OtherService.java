cf82ecc384ea9a38d699830c278ec747d1454819
HanDaYeon-coder <hdy78901@naver.com>
2023-07-26 18:13:48 +0900

    [DAD-485] fix: 기타 스크랩 저장시 사용하지 않는 siteName 삭제

--- src/main/java/com/forever/dadamda/service/scrap/OtherService.java
+++ src/main/java/com/forever/dadamda/service/scrap/OtherService.java
@@ -1,6 +1,5 @@
 package com.forever.dadamda.service.scrap;
 
-import com.forever.dadamda.entity.scrap.Article;
 import com.forever.dadamda.entity.scrap.Other;
 import com.forever.dadamda.entity.user.User;
 import com.forever.dadamda.repository.OtherRepository;
@@ -20,8 +19,7 @@ public class OtherService {
         Other other = Other.builder().user(user).pageUrl(pageUrl)
                 .title(crawlingResponse.get("title").toString())
                 .thumbnailUrl(crawlingResponse.get("thumbnail_url").toString())
-                .description(crawlingResponse.get("description").toString())
-                .siteName(crawlingResponse.get("site_name").toString()).build();
+                .description(crawlingResponse.get("description").toString()).build();
 
         return otherRepository.save(other);
     }
