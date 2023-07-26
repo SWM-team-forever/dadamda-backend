cd4f14936d8902c93372cd4838d7a16754b17b8f
Smilejune <godhkekf24@inha.edu>
2023-07-26 16:34:01 +0900

    [DAD-471] refactor: formatter 적용을 통한 코드 정리

--- src/main/java/com/forever/dadamda/entity/scrap/Other.java
+++ src/main/java/com/forever/dadamda/entity/scrap/Other.java
@@ -1,17 +1,20 @@
 package com.forever.dadamda.entity.scrap;
 
-import javax.persistence.Entity;
-
 import com.forever.dadamda.entity.user.User;
-import lombok.*;
+import javax.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class Other extends Scrap{
+public class Other extends Scrap {
 
     @Builder
-    public Other(User user, String pageUrl, String title, String thumbnailUrl, String description, String siteName) {
+    public Other(User user, String pageUrl, String title, String thumbnailUrl, String description,
+            String siteName) {
         super(user, pageUrl, title, thumbnailUrl, description, siteName);
     }
 }
