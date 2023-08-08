5abadc8737b98730c520a6a8a2625b32f27a2b9e
Smilejune <godhkekf24@inha.edu>
2023-08-08 14:46:56 +0900

    [DAD-568] refactor: 스크랩 추가시 일정 형태의 datetime을 파싱해서 사용하는 부분을 Unixtime을 받는 로직으로 변경

--- src/main/java/com/forever/dadamda/service/TimeService.java
+++ src/main/java/com/forever/dadamda/service/TimeService.java
@@ -1,13 +1,14 @@
 package com.forever.dadamda.service;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TimeService{
-    public static LocalDateTime parseToLocalDateTime(String time) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        return LocalDateTime.parse(time, formatter);
+    public static LocalDateTime fromUnixTime(long unixTime) {
+        return LocalDateTime.ofInstant(Instant.ofEpochSecond(unixTime), ZoneOffset.UTC);
     }
 }
