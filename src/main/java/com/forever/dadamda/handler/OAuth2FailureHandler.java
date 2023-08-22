220e8791bede79b630f14c56f7f070f9386ed91a
Smilejune <godhkekf24@inha.edu>
2023-08-22 16:23:17 +0900

    [DAD-721] refactor: google oauth 관련  log 추가

--- src/main/java/com/forever/dadamda/handler/OAuth2FailureHandler.java
+++ src/main/java/com/forever/dadamda/handler/OAuth2FailureHandler.java
@@ -20,5 +20,6 @@ public class OAuth2FailureHandler implements AuthenticationFailureHandler {
         response.getWriter().write("소셜 로그인 실패! 서버 로그를 확인해주세요.");
         log.info("소셜 로그인에 실패했습니다. 에러 메시지 : {} {}", exception.getMessage(),
                 exception.getStackTrace());
+        log.info(String.valueOf(exception.getCause()));
     }
 }
--- src/main/java/com/forever/dadamda/service/user/CustomOAuth2UserService.java
+++ src/main/java/com/forever/dadamda/service/user/CustomOAuth2UserService.java
@@ -27,14 +27,18 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 
         OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+        log.info("loadUser userRequest: {}", userRequest);
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
-
+        log.info("loadUser oAuth2User: {}", oAuth2User.getAttributes());
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        log.info("loadUser registrationId: {}", registrationId);
         String userNameAttributeName = userRequest.getClientRegistration()
                 .getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+        log.info("loadUser userNameAttributeName: {}", userNameAttributeName);
 
         OAuthAttributes attributes = OAuthAttributes.of(registrationId,
                 userNameAttributeName, oAuth2User.getAttributes());
+        log.info("loadUser attributes: {}", attributes.getAttributes());
 
         User user = saveOrUpdate(attributes);
         log.info("loadUser user: {}", user);
@@ -48,6 +52,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         User user = userRepository.findByEmailAndDeletedDateIsNull(attributes.getEmail())
                 .map(entity -> entity.update(attributes.getName(), attributes.getProfileUrl()))
                 .orElse(attributes.toEntity());
+        log.info("saveOrUpdate user: {}", user);
         return userRepository.save(user);
     }
 }
