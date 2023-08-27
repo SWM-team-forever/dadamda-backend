080b1e9baecc6587844813af47ecb23310605cec
HanDaYeon-coder <hdy78901@naver.com>
2023-08-27 20:36:55 +0900

    [DAD-710] chore: redis에 대한 의존성 제거
    테스트 코드 작성시, redis에 대한 테스트 환경을 구축해야 한다. 따라서, 해당 환경을 제외한 나머지에 대한 테스트를 작성하기 위해서 의존성을 제거한다.

--- src/main/java/com/forever/dadamda/DadamdaApplication.java
+++ src/main/java/com/forever/dadamda/DadamdaApplication.java
@@ -3,10 +3,8 @@ package com.forever.dadamda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableJpaAuditing
-@EnableRedisHttpSession
 @SpringBootApplication
 public class DadamdaApplication {
 
