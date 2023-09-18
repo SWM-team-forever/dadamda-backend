79534657523c8f56fb000a088133bf075fef4951
HanDaYeon-coder <hdy78901@naver.com>
2023-09-18 14:58:14 +0900

    [DAD-833] fix: fromUnixTime null 에러 처리

--- src/main/java/com/forever/dadamda/service/TimeService.java
+++ src/main/java/com/forever/dadamda/service/TimeService.java
@@ -3,12 +3,14 @@ package com.forever.dadamda.service;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TimeService{
-    public static LocalDateTime fromUnixTime(long unixTime) {
+    public static LocalDateTime fromUnixTime(Long unixTime) {
+        if(unixTime == null) {
+            return null;
+        }
         return LocalDateTime.ofInstant(Instant.ofEpochSecond(unixTime), ZoneOffset.UTC);
     }
 
