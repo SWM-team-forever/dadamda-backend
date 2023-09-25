077b839911ada45016477aa1ca00b10ce4e47bde
HanDaYeon-coder <hdy78901@naver.com>
2023-09-25 17:38:50 +0900

    [DAD-842] style: TAG의 설명 부분 수정

--- src/main/java/com/forever/dadamda/entity/board/TAG.java
+++ src/main/java/com/forever/dadamda/entity/board/TAG.java
@@ -8,10 +8,10 @@ import lombok.Getter;
 
 @Getter
 public enum TAG {
-    ENTERTAINMENT_ART("엔터테인먼트, 예술"),
-    LIFE_SHOPPING("생활, 노하우, 쇼핑"),
-    HOBBY_TRAVEL("취미, 여가, 여행"),
-    KNOWLEDGE_TREND("지식, 동향");
+    ENTERTAINMENT_ART("엔터테인먼트/예술"),
+    LIFE_SHOPPING("생활/노하우/쇼핑"),
+    HOBBY_TRAVEL("취미/여가/여행"),
+    KNOWLEDGE_TREND("지식/동향");
 
     private final String tagDescription;
 
