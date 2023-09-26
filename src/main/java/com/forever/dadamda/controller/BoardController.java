90593dc22646c0fdfee9262b66a6eabd9cf3e3c0
HanDaYeon-coder <hdy78901@naver.com>
2023-09-26 14:52:25 +0900

    [DAD-841] feat : 보드 삭제하기 API 구현

--- src/main/java/com/forever/dadamda/controller/BoardController.java
+++ src/main/java/com/forever/dadamda/controller/BoardController.java
@@ -5,12 +5,17 @@ import com.forever.dadamda.dto.board.CreateBoardRequest;
 import com.forever.dadamda.service.BoardService;
 import io.swagger.v3.oas.annotations.Operation;
 import javax.validation.Valid;
+import javax.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RequiredArgsConstructor
 @RestController
 public class BoardController {
@@ -24,6 +29,15 @@ public class BoardController {
             Authentication authentication) {
         String email = authentication.getName();
         boardService.createBoards(email, createBoardRequest);
+        return ApiResponse.success();
+    }
+
+    @Operation(summary = "보드 삭제", description = "1개의 보드를 삭제합니다.")
+    @DeleteMapping("/v1/boards/{boardId}")
+    public ApiResponse<String> createBoards(@PathVariable @Positive Long boardId,
+            Authentication authentication) {
+        String email = authentication.getName();
+        boardService.deleteBoards(email, boardId);
         return ApiResponse.success();
     }
 }
