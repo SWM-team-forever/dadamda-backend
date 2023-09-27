82654cacbbe91920da21547d236699e3bf645806
HanDaYeon-coder <hdy78901@naver.com>
2023-09-27 22:47:21 +0900

    [DAD-900] test: 장소 스크랩 조회시 삭제된 메모는 조회되지 않는지 확인하는 테스트 작성

--- src/test/resources/place-setup.sql
+++ src/test/resources/place-setup.sql
@@ -2,9 +2,16 @@
 INSERT INTO users (user_id, name, provider, role, email, profile_url)
 VALUES (1, 'koko', 0, 'USER', '1234@naver.com', 'https://www.naver.com');
 
--- Other 데이터 삽입
+-- Place 데이터 삽입
 INSERT INTO scrap (user_id, scrap_id, page_url, latitude, longitude, title, d_type, created_date)
 VALUES (1, 1, 'https://www.kakaomap.com/1', 37.00000000000000, 127.02969594506668, '서울 빌딩', 'Place', '2023-01-01 11:11:01');
 
 INSERT INTO scrap (user_id, scrap_id, page_url, latitude, longitude, title, d_type, created_date)
 VALUES (1, 2, 'https://www.kakaomap.com/1', 38.496490, 126.296959, '인천 빌딩', 'Place', '2023-01-02 11:11:01');
+
+-- 메모 데이터 삽입
+INSERT INTO memo (scrap_id, memo_id, memo_text, created_date)
+VALUES (2, 1, 'Hello 1', '2023-01-03 11:11:01');
+
+INSERT INTO memo (scrap_id, memo_id, memo_text, created_date, deleted_date)
+VALUES (2, 2, 'Hello 2', '2023-01-04 12:11:01', '2023-01-05 12:11:01');
