17542df53cc09762b6f911e619f5f8c6530617b6
HanDaYeon-coder <hdy78901@naver.com>
2023-09-27 19:57:26 +0900

    [DAD-900] fix: 삭제된 메모는 조회되지 않도록 스크랩 조회, 검색 부분 수정

--- src/main/java/com/forever/dadamda/repository/MemoRepository.java
+++ src/main/java/com/forever/dadamda/repository/MemoRepository.java
@@ -2,12 +2,13 @@ package com.forever.dadamda.repository;
 
 import com.forever.dadamda.entity.Memo;
 import com.forever.dadamda.entity.scrap.Scrap;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
 
-    Optional<Memo> findByIdAndDeletedDateIsNull(Long memoId);
+    List<Memo> findMemosByScrapAndDeletedDateIsNull(Scrap scrap);
 
     Optional<Memo> findMemoByIdAndScrapAndDeletedDateIsNull(Long memoId, Scrap scrap);
 }
