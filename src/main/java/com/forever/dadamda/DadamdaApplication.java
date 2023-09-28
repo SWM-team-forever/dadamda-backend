f8fe9f898f1a0380eb7c172f13c578f3e9b10c1f
Smilejune <godhkekf24@inha.edu>
2023-09-28 14:59:20 +0900

    [DAD-875] refactor: 서버 기준 시간 UTC로 변경

--- src/main/java/com/forever/dadamda/DadamdaApplication.java
+++ src/main/java/com/forever/dadamda/DadamdaApplication.java
@@ -1,10 +1,18 @@
 package com.forever.dadamda;
 
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DadamdaApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DadamdaApplication.class, args);
