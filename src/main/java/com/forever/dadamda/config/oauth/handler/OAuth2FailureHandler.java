adb3983791012d3494219c3fbb0dfc0d53d81b54
HanDaYeon-coder <hdy78901@naver.com>
2023-10-03 16:20:23 +0900

    [DAD-910] refactor: 사용하지 않는 REDIRECT_URI_PARAM_COOKIE_NAME 제거

--- src/main/java/com/forever/dadamda/config/oauth/handler/OAuth2FailureHandler.java
+++ src/main/java/com/forever/dadamda/config/oauth/handler/OAuth2FailureHandler.java
@@ -1,17 +1,15 @@
 package com.forever.dadamda.config.oauth.handler;
 
-import static com.forever.dadamda.config.oauth.HttpCookieOAuth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.forever.dadamda.config.oauth.HttpCookieOAuth2AuthorizationRequestRepository;
-import com.forever.dadamda.config.oauth.util.CookieUtils;
+import com.forever.dadamda.dto.ErrorCode;
+import com.forever.dadamda.dto.JwtErrorResponse;
+import io.sentry.Sentry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -24,17 +22,21 @@ public class OAuth2FailureHandler extends SimpleUrlAuthenticationFailureHandler 
     private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
 
     @Override
-    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        String targetUrl = CookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
-                .map(Cookie::getValue)
-                .orElse(("/"));
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
 
-        targetUrl = UriComponentsBuilder.fromUriString(targetUrl)
-                .queryParam("error", exception.getLocalizedMessage())
-                .build().toUriString();
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("application/json");
+
+        JwtErrorResponse jwtErrorResponse = new JwtErrorResponse(
+                ErrorCode.INVALID_AUTH_TOKEN.getCode(), ErrorCode.INVALID_AUTH_TOKEN.getMessage());
+        ObjectMapper objectMapper = new ObjectMapper();
+        String result = objectMapper.writeValueAsString(jwtErrorResponse);
+
+        Sentry.captureException(exception);
+
+        response.getWriter().write(result);
 
         httpCookieOAuth2AuthorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
-
-        getRedirectStrategy().sendRedirect(request, response, targetUrl);
     }
 }
