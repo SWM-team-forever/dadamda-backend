84848116df7bb3b8c4a574a19d78d2bcc107845d
HanDaYeon-coder <hdy78901@naver.com>
2023-10-10 14:57:28 +0900

    [DAD-933] test: 보드 내용 수정 API 테스트 코드 작성

--- src/test/java/com/forever/dadamda/service/BoardServiceTest.java
+++ src/test/java/com/forever/dadamda/service/BoardServiceTest.java
@@ -1,9 +1,11 @@
 package com.forever.dadamda.service;
 
+import static com.forever.dadamda.entity.board.TAG.LIFE_SHOPPING;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import com.forever.dadamda.dto.board.CreateBoardRequest;
 import com.forever.dadamda.dto.board.GetBoardResponse;
+import com.forever.dadamda.dto.board.UpdateBoardRequest;
 import com.forever.dadamda.entity.board.Board;
 import com.forever.dadamda.entity.user.User;
 import com.forever.dadamda.repository.board.BoardRepository;
@@ -108,5 +110,50 @@ public class BoardServiceTest {
         assertThat(getBoardResponseSlice.getContent().get(1).getBoardId()).isEqualTo(2L);
         assertThat(getBoardResponseSlice.getContent().get(2).getBoardId()).isEqualTo(3L);
         assertThat(getBoardResponseSlice.getContent().get(3).getBoardId()).isEqualTo(1L);
+    }
+
+    @Test
+    void should_the_tag_name_description_modified_date_are_modified_successfully_When_modifying_a_board() {
+        // 보드를 수정할 때, tag, name, description, modified_date가 성공적으로 수정되는지 확인
+        //given
+        Long bordId = 1L;
+        UpdateBoardRequest updateBoardRequest = UpdateBoardRequest.builder()
+                .tag("LIFE_SHOPPING")
+                .name("test")
+                .description("test123")
+                .build();
+
+        //when
+        boardService.updateBoards(existentEmail, bordId, updateBoardRequest);
+
+        //then
+        Board board = boardRepository.findById(bordId).get();
+
+        assertThat(board.getModifiedDate()).isAfter(
+                LocalDateTime.of(2023, 1, 2, 11, 11, 1));
+        assertThat(board.getTag()).isEqualTo(LIFE_SHOPPING);
+        assertThat(board.getName()).isEqualTo("test");
+        assertThat(board.getDescription()).isEqualTo("test123");
+    }
+
+    @Test
+    void should_it_is_not_modified_it_is_the_same_as_the_previous_one_When_modifying_a_board() {
+        // 보드를 수정할 때, 이전 내용과 동일하다면, 수정되지 않는지 확인
+        //given
+        Long bordId = 1L;
+        UpdateBoardRequest updateBoardRequest = UpdateBoardRequest.builder()
+                .tag("ENTERTAINMENT_ART")
+                .name("board1")
+                .description("test")
+                .build();
+
+        //when
+        boardService.updateBoards(existentEmail, bordId, updateBoardRequest);
+
+        //then
+        Board board = boardRepository.findById(bordId).get();
+
+        assertThat(board.getModifiedDate()).isEqualTo(
+                LocalDateTime.of(2023, 1, 1, 11, 11, 1));
     }
 }
