0c723bcc2a2a032381c4619541eab4778afcb94c
HanDaYeon-coder <hdy78901@naver.com>
2023-10-12 01:11:40 +0900

    [DAD-940] refactor: 생성자 어노테이션 추가

--- src/main/java/com/forever/dadamda/dto/board/UpdateBoardContentsRequest.java
+++ src/main/java/com/forever/dadamda/dto/board/UpdateBoardContentsRequest.java
@@ -1,9 +1,13 @@
 package com.forever.dadamda.dto.board;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class UpdateBoardContentsRequest {
 
