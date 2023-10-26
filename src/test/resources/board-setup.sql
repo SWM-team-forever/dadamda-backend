c23b46d96abc48b963205e2b1d715c9418a374dd
HanDaYeon-coder <hdy78901@naver.com>
2023-10-27 01:50:13 +0900

    [DAD-1051] fix: 유저 생성시 랜덤 닉네임 속성값 추가

--- src/test/resources/board-setup.sql
+++ src/test/resources/board-setup.sql
@@ -1,6 +1,6 @@
 -- User 데이터 삽입
-INSERT INTO users (user_id, name, provider, role, email, profile_url)
-VALUES (1, 'koko', 0, 'USER', '1234@naver.com', 'https://www.naver.com');
+INSERT INTO users (user_id, name, provider, role, email, profile_url, nickname)
+VALUES (1, 'koko', 0, 'USER', '1234@naver.com', 'https://www.naver.com', '귀여운해달1');
 
 INSERT INTO users (user_id, name, provider, role, email, profile_url)
 VALUES (2, 'coco', 0, 'USER', '12345@naver.com', 'https://www.naver.com');
