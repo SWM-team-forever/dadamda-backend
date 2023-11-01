0430ec7f38cfa56d5b87c571b7ef94e4e5cc0dcc
ILJUN YUN <91049936+SmileJune@users.noreply.github.com>
2023-11-01 14:57:32 +0900

    [d

--- src/main/java/com/forever/dadamda/controller/UserController.java
+++ src/main/java/com/forever/dadamda/controller/UserController.java
@@ -62,6 +62,8 @@ public class UserController {
     public ApiResponse uploadProfileImage(Authentication authentication, @RequestParam("file") MultipartFile file) {
         String email = authentication.getName();
         userService.uploadProfileImage(email, file);
+
+        return ApiResponse.success();
     }
 
     @Operation(summary = "회원 닉네임 수정", description = "해당 회원 닉네임를 수정할 수 있습니다.")
