24a7de62b3f7333758d9301b1d99bd959b6b560a
Smilejune <godhkekf24@inha.edu>
2023-11-02 23:08:54 +0900

    [DAD-1121] refactor: 보드 디테일 조회 시 속성 값 추가
    boardUUID, isPublic, isShared, fixedDate, heartCnt, viewCnt, shareCnt, thumbnailUrl 추가

--- src/main/java/com/forever/dadamda/dto/board/GetBoardDetailResponse.java
+++ src/main/java/com/forever/dadamda/dto/board/GetBoardDetailResponse.java
@@ -2,6 +2,7 @@ package com.forever.dadamda.dto.board;
 
 import com.forever.dadamda.entity.board.Board;
 import com.forever.dadamda.entity.board.TAG;
+import com.forever.dadamda.service.TimeService;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,16 +11,32 @@ import lombok.Getter;
 public class GetBoardDetailResponse {
 
     private Long boardId;
+    private String boardUUID;
     private String title;
+    private boolean isPublic;
+    private boolean isShared;
+    private Long fixedDate;
     private String description;
     private TAG tag;
+    private Long heartCnt;
+    private Long viewCnt;
+    private Long shareCnt;
+    private String thumbnailUrl;
 
     public static GetBoardDetailResponse of(Board board) {
         return GetBoardDetailResponse.builder()
                 .boardId(board.getId())
+                .boardUUID(board.getUuid().toString())
                 .title(board.getTitle())
+                .isPublic(board.isPublic())
+                .isShared(board.isShared())
+                .fixedDate(TimeService.fromLocalDateTime(board.getFixedDate()))
                 .description(board.getDescription())
                 .tag(board.getTag())
+                .heartCnt(board.getHeartCnt())
+                .viewCnt(board.getViewCnt())
+                .shareCnt(board.getShareCnt())
+                .thumbnailUrl(board.getThumbnailUrl())
                 .build();
     }
 }
