5b268edecfdfca30897b0501e35d6159db16916e
HanDaYeon-coder <hdy78901@naver.com>
2023-11-07 00:53:43 +0900

    [DAD-1191] test: 공유된 보드 복사시, 공유수 증가 및 원작자 속성을 원작 속성으로 변경에 따른 테스트 변경 및 작성

--- src/test/resources/board-setup.sql
+++ src/test/resources/board-setup.sql
@@ -6,17 +6,17 @@ INSERT INTO users (user_id, name, provider, role, email, profile_url, nickname, 
 VALUES (2, 'coco', 0, 'USER', '12345@naver.com', 'https://www.naver.com', '귀여운해달2', '0782ef48-a439-02');
 
 -- Board 데이터 삽입
-INSERT INTO board (user_id, board_id, title, description, uuid, tag, created_date, is_public, is_shared, modified_date, authorship_id, thumbnail_url, heart_cnt, view_cnt, share_cnt)
-VALUES (1, 1, 'board1', 'test', '0782ef48-a439-11', 0 ,'2023-01-01 11:11:01', 1, 1, '2023-01-01 11:11:01', 1, 'board1 thumbnail url', 10, 11, 12);
+INSERT INTO board (user_id, board_id, title, description, uuid, tag, created_date, is_public, is_shared, modified_date, thumbnail_url, heart_cnt, view_cnt, share_cnt)
+VALUES (1, 1, 'board1', 'test', '0782ef48-a439-11', 0 ,'2023-01-01 11:11:01', 1, 1, '2023-01-01 11:11:01', 'board1 thumbnail url', 10, 11, 12);
 
-INSERT INTO board (user_id, board_id, title, description, uuid, tag, created_date, is_public, modified_date, fixed_date, contents, authorship_id)
-VALUES (1, 2, 'board2', 'test', '0782ef48-a439-12', 1 ,'2023-01-01 11:11:01', 0, '2023-01-02 11:11:01', '2023-01-03 11:11:01', 'test contents', 1);
+INSERT INTO board (user_id, board_id, title, description, uuid, tag, created_date, is_public, modified_date, fixed_date, contents, heart_cnt, view_cnt, share_cnt)
+VALUES (1, 2, 'board2', 'test', '0782ef48-a439-12', 1 ,'2023-01-01 11:11:01', 0, '2023-01-02 11:11:01', '2023-01-03 11:11:01', 'test contents', 0, 0, 0);
 
-INSERT INTO board (user_id, board_id, title, description, uuid, tag, created_date, is_public, is_shared, modified_date, contents, authorship_id, heart_cnt, view_cnt, share_cnt)
-VALUES (1, 3, 'board3', 'test', '0782ef48-a439-13', 1 ,'2023-01-01 11:11:01', 1, 1, '2023-01-03 11:11:01', 'test contents3', 1, 10, 11, 13);
+INSERT INTO board (user_id, board_id, title, description, uuid, tag, created_date, is_public, is_shared, modified_date, contents, heart_cnt, view_cnt, share_cnt)
+VALUES (1, 3, 'board3', 'test', '0782ef48-a439-13', 1 ,'2023-01-01 11:11:01', 1, 1, '2023-01-03 11:11:01', 'test contents3', 10, 11, 13);
 
-INSERT INTO board (user_id, board_id, title, description, uuid, tag, created_date, is_public, modified_date, fixed_date, authorship_id, heart_cnt, view_cnt, share_cnt)
-VALUES (1, 4, 'board4', 'test', '0782ef48-a439-14', 2 ,'2023-01-01 11:11:01', 1, '2023-01-04 11:11:01', '2023-01-04 11:11:01', 1, 9, 11, 14);
+INSERT INTO board (user_id, board_id, title, description, uuid, tag, created_date, is_public, modified_date, fixed_date, heart_cnt, view_cnt, share_cnt)
+VALUES (1, 4, 'board4', 'test', '0782ef48-a439-14', 2 ,'2023-01-01 11:11:01', 1, '2023-01-04 11:11:01', '2023-01-04 11:11:01', 9, 11, 14);
 
-INSERT INTO board (user_id, board_id, title, description, uuid, tag, created_date, is_public, modified_date, deleted_date, authorship_id, heart_cnt, view_cnt, share_cnt)
-VALUES (1, 5, 'board5', 'test', '0782ef48-a439-15', 0 ,'2023-01-01 11:11:01', 1, '2023-01-04 11:11:01', '2023-01-04 11:11:01', 1, 6, 11, 12);
+INSERT INTO board (user_id, board_id, title, description, uuid, tag, created_date, is_public, modified_date, deleted_date, heart_cnt, view_cnt, share_cnt)
+VALUES (1, 5, 'board5', 'test', '0782ef48-a439-15', 0 ,'2023-01-01 11:11:01', 1, '2023-01-04 11:11:01', '2023-01-04 11:11:01', 6, 11, 12);
