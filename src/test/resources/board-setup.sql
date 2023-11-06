f74fd3c19e06dba95c82ff476ef4fdc2226c60ca
HanDaYeon-coder <hdy78901@naver.com>
2023-11-06 22:01:27 +0900

    [DAD-1189] test: 보드 목록 조회시 응답 값에 보드의 컨텐츠 추가에 따른 테스트 추가

--- src/test/resources/board-setup.sql
+++ src/test/resources/board-setup.sql
@@ -15,8 +15,8 @@ VALUES (1, 2, 'board2', 'test', '0782ef48-a439-12', 1 ,'2023-01-01 11:11:01', 0,
 INSERT INTO board (user_id, board_id, title, description, uuid, tag, created_date, is_public, is_shared, modified_date, contents, authorship_id, heart_cnt, view_cnt, share_cnt)
 VALUES (1, 3, 'board3', 'test', '0782ef48-a439-13', 1 ,'2023-01-01 11:11:01', 1, 1, '2023-01-03 11:11:01', 'test contents3', 1, 10, 11, 13);
 
-INSERT INTO board (user_id, board_id, title, description, uuid, tag, created_date, is_public, modified_date, fixed_date, authorship_id, heart_cnt, view_cnt, share_cnt)
-VALUES (1, 4, 'board4', 'test', '0782ef48-a439-14', 2 ,'2023-01-01 11:11:01', 1, '2023-01-04 11:11:01', '2023-01-04 11:11:01', 1, 9, 11, 14);
+INSERT INTO board (user_id, board_id, title, description, uuid, tag, created_date, is_public, modified_date, fixed_date, authorship_id, heart_cnt, view_cnt, share_cnt, contents)
+VALUES (1, 4, 'board4', 'test', '0782ef48-a439-14', 2 ,'2023-01-01 11:11:01', 1, '2023-01-04 11:11:01', '2023-01-04 11:11:01', 1, 9, 11, 14, 'test contents4');
 
 INSERT INTO board (user_id, board_id, title, description, uuid, tag, created_date, is_public, modified_date, deleted_date, authorship_id, heart_cnt, view_cnt, share_cnt)
 VALUES (1, 5, 'board5', 'test', '0782ef48-a439-15', 0 ,'2023-01-01 11:11:01', 1, '2023-01-04 11:11:01', '2023-01-04 11:11:01', 1, 6, 11, 12);
