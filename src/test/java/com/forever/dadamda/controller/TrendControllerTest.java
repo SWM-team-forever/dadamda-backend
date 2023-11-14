e043bd5fd8c13d37613c2784a2d6bc6a75569e1f
HanDaYeon-coder <hdy78901@naver.com>
2023-11-14 16:41:31 +0900

    [DAD-1217] test: 내 트렌드 보드 조회 테스트 작성

--- src/test/java/com/forever/dadamda/controller/TrendControllerTest.java
+++ src/test/java/com/forever/dadamda/controller/TrendControllerTest.java
@@ -152,4 +152,45 @@ public class TrendControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data[1].nickname").value("귀여운해달2"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data[2].nickname").value("귀여운해달1"));
     }
+
+    @Test
+    @WithCustomMockUser
+    public void should_it_is_arranged_in_the_order_of_heart_share_view_and_only_my_board_is_gotten_When_getting_my_trending_boards()
+            throws Exception {
+        // 트랜딩 보드에 나의 게시된 보드를 조회할 때, 나의 보드만 조회되고, 하트 순, 공유 순, 조회 순으로 정렬된다.
+        mockMvc.perform(get("/v1/trends/boards")
+                        .param("startDate", "2023-01-01 00:00:00")
+                        .param("endDate", "2023-03-31 23:59:59")
+                        .param("page", "0")
+                        .param("size", "10")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-AUTH-TOKEN", "aaaaaaa")
+                )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.content[0].nickname").value("귀여운해달1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.content[0].nickname").value("귀여운해달1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.content[0].nickname").value("귀여운해달1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.content[0].title").value("board3"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.content[1].title").value("board1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.content[2].title").value("board4"));
+    }
+
+    @Test
+    @WithCustomMockUser
+    public void should_only_the_boards_of_that_tag_are_returned_When_getting_my_trending_boards_with_tags()
+            throws Exception {
+        // 트랜딩 보드에 나의 게시된 보드를 조회할 때, 태그를 입력하면 해당 태그의 보드만 조회된다.
+        mockMvc.perform(get("/v1/trends/boards")
+                        .param("startDate", "2023-01-01 00:00:00")
+                        .param("endDate", "2023-03-31 23:59:59")
+                        .param("page", "0")
+                        .param("size", "10")
+                        .param("tag", "ENTERTAINMENT_ART")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-AUTH-TOKEN", "aaaaaaa")
+                )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.content[0].title").value("board1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.content[1]").doesNotExist());
+    }
 }
